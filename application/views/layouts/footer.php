6148851245e250e4a6758143d8ff21ac37a5aaf9
unknown <jemahechaj@ut.edu.co>
2019-03-06 15:45:22 -0500

    Creacion de funciones para rellenar campos de comprobante

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -115,7 +115,50 @@
         $('#example1').DataTable();
         $('.sidebar-menu').tree();
 
+        /*#comprobantes */
+        $("#comprobantes").on("change", function() {
+
+            option = $(this).val();
+            infoComprobante = option.split("*");
+
+            if (option != "") {
+                $("#idcomprobante").val(infoComprobante[0]);
+                $("#iva").val(infoComprobante[3]);
+                $("#serie").val(infoComprobante[4]);
+                $('#numero').val(generarNumero(infoComprobante[2]));
+            } else {
+                $("#idcomprobante").val(null);
+                $("#iva").val(null);
+                $("#serie").val(null);
+                $('#numero').val(null);
+            }
+
+
+
+
+        })
     })
+
+    function generarNumero(numero) {
+        if (numero >= 99999 && numero < 999999) {
+            return Number(numero) + 1
+        }
+        if (numero >= 9999 && numero < 99999) {
+            return "0" + (Number(numero)) + 1
+        }
+        if (numero >= 999 && numero < 9999) {
+            return "00" + (Number(numero) + 1)
+        }
+        if (numero >= 99 && numero < 999) {
+            return "000" + (Number(numero) + 1)
+        }
+        if (numero >= 9 && numero < 99) {
+            return "0000" + (Number(numero) + 1)
+        }
+        if (numero < 9) {
+            return "00000" + (Number(numero) + 1)
+        }
+    }
 </script>
 </body>
 
